827684caaf5b0d64efd3383831a7ea942f06a577
ashmashrest <ashma.shrestha@gmail.com>
2014-09-08 16:31:11 -0500

    remove authentication action handled by shibboleth

--- module/Dds/src/Dds/Controller/AuthController.php
+++ module/Dds/src/Dds/Controller/AuthController.php
@@ -60,45 +60,7 @@ class AuthController extends AbstractActionController
         }
      
         return new ViewModel(array('logged'=> $logged));
-    }
-     
-    public function authenticateAction()
-    {
-        /*$form       = $this->getForm();
-        $redirect = 'login';
-         
-        $request = $this->getRequest();
-        if ($request->isPost()){
-            $form->setData($request->getPost());
-            if ($form->isValid()){
-                //check authentication...
-                $this->getAuthService()->getAdapter()
-                                       ->setIdentity($request->getPost('username'))
-                                       ->setCredential($request->getPost('password'));
-                                        
-                $result = $this->getAuthService()->authenticate();
-                foreach($result->getMessages() as $message)
-                {
-                    //save message temporary into flashmessenger
-                    $this->flashmessenger()->addMessage($message);
-                }
-                 
-                if ($result->isValid()) {
-                    $redirect = 'success';
-                    //check if it has rememberMe :
-                    if ($request->getPost('rememberme') == 1 ) {
-                        $this->getSessionStorage()
-                             ->setRememberMe(1);
-                        //set storage again 
-                        $this->getAuthService()->setStorage($this->getSessionStorage());
-                    }
-                    $this->getAuthService()->getStorage()->write($request->getPost('username'));
-                }
-            }
-        }
-         
-        return $this->redirect()->toRoute($redirect);*/
-    }
+    }     
      
     public function logoutAction()
     {
